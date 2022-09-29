79bf939e166a4ad74d1f1fb9325e30e7fcedffd0
RicardoTejedorRestrepo <ricardo.tejedor.restrepo@unillanos.edu.co>
2022-09-28 22:02:30 -0500

    HachiFit3

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -20,11 +20,7 @@ class _HomePageState extends State<HomePage> {
 
   List<String> _lista = [];
 
-  List<String> _lista_Filtro = [
-    'Body Part',
-    'Equipment',
-    'Target'
-  ];
+  List<String> _lista_Filtro = ['Body Part', 'Equipment', 'Target'];
 
   List<String> _lista_BodyPart = [
     'back',
@@ -40,59 +36,61 @@ class _HomePageState extends State<HomePage> {
   ];
 
   List<String> _lista_Equipmennt = [
-  "assisted",
-  "band",
-  "barbell",
-  "body weight",
-  "bosu ball",
-  "cable",
-  "dumbbell",
-  "elliptical machine",
-  "ez barbell",
-  "hammer",
-  "kettlebell",
-  "leverage machine",
-  "medicine ball",
-  "olympic barbell",
-  "resistance band",
-  "roller",
-  "rope",
-  "skierg machine",
-  "sled machine",
-  "smith machine",
-  "stability ball",
-  "stationary bike",
-  "stepmill machine",
-  "tire",
-  "trap bar",
-  "upper body ergometer",
-  "weighted",
-  "wheel roller"
+    "assisted",
+    "band",
+    "barbell",
+    "body weight",
+    "bosu ball",
+    "cable",
+    "dumbbell",
+    "elliptical machine",
+    "ez barbell",
+    "hammer",
+    "kettlebell",
+    "leverage machine",
+    "medicine ball",
+    "olympic barbell",
+    "resistance band",
+    "roller",
+    "rope",
+    "skierg machine",
+    "sled machine",
+    "smith machine",
+    "stability ball",
+    "stationary bike",
+    "stepmill machine",
+    "tire",
+    "trap bar",
+    "upper body ergometer",
+    "weighted",
+    "wheel roller"
   ];
 
   List<String> _lista_Target = [
-  "abductors",
-  "abs",
-  "adductors",
-  "biceps",
-  "calves",
-  "cardiovascular system",
-  "delts",
-  "forearms",
-  "glutes",
-  "hamstrings",
-  "lats",
-  "levator scapulae",
-  "pectorals",
-  "quads",
-  "serratus anterior",
-  "spine",
-  "traps",
-  "triceps",
-  "upper back"
+    "abductors",
+    "abs",
+    "adductors",
+    "biceps",
+    "calves",
+    "cardiovascular system",
+    "delts",
+    "forearms",
+    "glutes",
+    "hamstrings",
+    "lats",
+    "levator scapulae",
+    "pectorals",
+    "quads",
+    "serratus anterior",
+    "spine",
+    "traps",
+    "triceps",
+    "upper back"
   ];
   String _vista_Filtro = 'Target';
   String _vista = 'abs';
+
+  String valor = "";
 
   void initState() {
     //articles = articuloProvider.getArticles();
@@ -116,7 +114,7 @@ class _HomePageState extends State<HomePage> {
                   children: [
                     Expanded(
                         child: Padding(
-                      padding: const EdgeInsets.all(15),
+                      padding: const EdgeInsets.all(10),
                       child: Container(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -133,7 +131,7 @@ class _HomePageState extends State<HomePage> {
                     )),
                     Expanded(
                         child: Padding(
-                      padding: const EdgeInsets.all(15),
+                      padding: const EdgeInsets.all(10),
                       child: Container(
                         child: Column(
                           mainAxisAlignment: MainAxisAlignment.center,
@@ -141,52 +139,66 @@ class _HomePageState extends State<HomePage> {
                             Text(
                               "Filter by:",
                               style: TextStyle(
-                                  fontWeight: FontWeight.bold, fontSize: 30),
+                                  fontWeight: FontWeight.bold, fontSize: 20),
                             ),
                             DropdownButton(
                               items: _lista_Filtro.map((String a) {
                                 return DropdownMenuItem(
-                                    value: a, child: Text(a, style: TextStyle(fontSize: 20),));
+                                    value: a,
+                                    child: Text(
+                                      a,
+                                      style: TextStyle(fontSize: 15),
+                                    ));
                               }).toList(),
                               onChanged: (_value) => {
                                 setState(() {
                                   _vista_Filtro = _value.toString();
-                                  if(_vista_Filtro == 'Target'){
+                                  if (_vista_Filtro == 'Target') {
                                     _lista = _lista_Target;
                                   }
-                                  if(_vista_Filtro == 'Equipment'){
+                                  if (_vista_Filtro == 'Equipment') {
                                     _lista = _lista_Equipmennt;
                                   }
-                                  if(_vista_Filtro == 'Body Part'){
+                                  if (_vista_Filtro == 'Body Part') {
                                     _lista = _lista_BodyPart;
                                   }
                                 })
                               },
-                              hint: Text(_vista_Filtro, style: TextStyle(fontSize: 25),),
+                              hint: Text(
+                                _vista_Filtro,
+                                style: TextStyle(fontSize: 20),
+                              ),
                             ),
                             DropdownButton(
                               items: _lista.map((String a) {
                                 return DropdownMenuItem(
-                                    value: a, child: Text(a, style: TextStyle(fontSize: 20),));
+                                    value: a,
+                                    child: Text(
+                                      a,
+                                      style: TextStyle(fontSize: 15),
+                                    ));
                               }).toList(),
                               onChanged: (_value) => {
                                 setState(() {
                                   _vista = _value.toString();
-                                  if(_vista_Filtro == 'Target'){
+                                  if (_vista_Filtro == 'Target') {
                                     exercises = exerciseProvider
-                                      .getArticlesbyTarget(_vista);
+                                        .getArticlesbyTarget(_vista);
                                   }
-                                  if(_vista_Filtro == 'Equipment'){
+                                  if (_vista_Filtro == 'Equipment') {
                                     exercises = exerciseProvider
-                                      .getArticlesbyEquipment(_vista);
+                                        .getArticlesbyEquipment(_vista);
                                   }
-                                  if(_vista_Filtro == 'Body Part'){
+                                  if (_vista_Filtro == 'Body Part') {
                                     exercises = exerciseProvider
-                                      .getExercisebybodyPart(_vista);
+                                        .getExercisebybodyPart(_vista);
                                   }
                                 })
                               },
-                              hint: Text(_vista, style: TextStyle(fontSize: 25),),
+                              hint: Text(
+                                _vista,
+                                style: TextStyle(fontSize: 20),
+                              ),
                             )
                           ],
                         ),
@@ -199,6 +211,53 @@ class _HomePageState extends State<HomePage> {
                   ],
                 ),
               )),
+          //-------------------------------------------------------------------------
+          Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.all(5),
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //Text('Buscar por nombre'),
+                      Container(
+                          //height: 100,
+                          width: 300,
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                                child: TextField(
+                                    decoration: InputDecoration(
+                                      labelText: 'Search by name',
+                                      labelStyle: TextStyle(color: Colors.red, fontWeight: FontWeight.w600),
+                                      border: InputBorder.none,
+                                      fillColor: Colors.white60,
+                                      filled: true,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                    onChanged: (texto) {
+                                      valor = texto;
+                                      print(valor);
+                                      setState(() {
+                                        exercises = exerciseProvider.getArticlesbyName(valor);
+                                      });
+                                      
+                                    }),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 126, 126, 126),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              )),
+          //-------------------------------------------------------------------------
           Expanded(
             flex: 8,
             child: Container(
--- lib/providers/exercises.provider.dart
+++ lib/providers/exercises.provider.dart
@@ -143,4 +143,40 @@ class ExerciseProvider {
     }
   }
 
+  Future<List<ExerciseModel>> getArticlesbyName(String name) async {
+    List<ExerciseModel> articles = [];
+    var client = http.Client();
+
+    try {
+      var url = 'https://exercisedb.p.rapidapi.com/';
+      var apikey = "238da9a941msh584e731c85a6bcap1f8b86jsne4fc9e87f319";//apikey suarez
+      //var apikey ="b8c9f91f35msh4c441ffc0ba3643p1ae8d6jsn6f0672535fd9";//apikey Tejedor
+      
+      var endpoint = "exercises/name/$name";
+      
+      var uri = Uri.parse(url + endpoint);
+      var response = await http.get(uri, headers:{'X-RapidAPI-Key':apikey});
+      //print("aqui el response");
+      print(response.statusCode);
+      
+      var decodedResponse =
+          jsonDecode(utf8.decode(response.bodyBytes));
+      
+      decodedResponse 
+          .forEach((item) => articles.add(ExerciseModel.fromJson(item)));
+      var ar1 = articles[1];
+      
+      //var a = await translator.translate(articles[1].bodypart, to: 'es');
+      //print(a);
+      return articles;
+    }  catch(e) { 
+      print(e);
+      
+      return articles;
+   } 
+     finally {
+      client.close();
+    }
+  }
+
 }
